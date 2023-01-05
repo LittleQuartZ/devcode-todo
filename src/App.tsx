aa5b742dc1ddcfcdf29158dbbd87dfd07d6e2a0f
Syahdan <58659387+LittleQuartZ@users.noreply.github.com>
2023-01-05 16:55:58 +0700

    add create feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { RiAddFill, RiLoader3Fill } from 'react-icons/ri';
 
 import Navbar from './components/Navbar';
-import { useQuery } from '@tanstack/react-query';
-import { getAllActivity } from './api';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { createActivity, getAllActivity } from './api';
 import ActivityItem from './components/ActivityItem';
 import EmptyPlaceholder from './components/EmptyPlaceholder';
 
 const App = () => {
+  const queryClient = useQueryClient();
   const activityGroups = useQuery({
     queryKey: ['activity-groups'],
     queryFn: getAllActivity,
@@ -14,6 +15,17 @@ const App = () => {
       alert((err as Error).message);
     },
   });
+  const addActivityGroup = useMutation({
+    mutationKey: ['activity-groups-create'],
+    mutationFn: createActivity,
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['activity-groups'] });
+    },
+  });
+
+  const onAddClick = async () => {
+    addActivityGroup.mutate();
+  };
 
   return (
     <>
@@ -24,10 +36,18 @@ const App = () => {
             Activity
           </h1>
           <button
+            onClick={onAddClick}
+            disabled={addActivityGroup.isLoading}
             data-cy='activity-add-button'
-            className='flex items-center font-bold gap-2 bg-primary rounded-full px-4 py-3 text-xs text-white'>
-            <RiAddFill className='w-4 h-4' />
-            Tambah
+            className='flex items-center font-bold gap-2 disabled:bg-black-3 bg-primary rounded-full px-4 py-3 text-xs text-white'>
+            {addActivityGroup.isLoading ? (
+              <RiLoader3Fill className='animate-spin w-4 h-4' />
+            ) : (
+              <>
+                <RiAddFill className='w-4 h-4' />
+                Tambah
+              </>
+            )}
           </button>
         </section>
         {activityGroups.data ? (
--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://todo.api.devcode.gethired.id';
+const EMAIL = 'syahdanhafizzz@gmail.com';
 
 export type Activity = {
   created_at: string;
@@ -17,11 +18,25 @@ type Response<T> = {
 
 export const getAllActivity = async () => {
   const response = await axios.get<Response<Activity[]>>(
-    `${BASE_URL}/activity-groups`
+    `${BASE_URL}/activity-groups?email=${EMAIL}`
   );
 
   if (response.status !== 200) {
     throw new Error(`Failed fetching activity groups: ${response.statusText}`);
+  }
+
+  return response.data.data;
+};
+
+export const createActivity = async () => {
+  const response = await axios.post<
+    Response<Activity & { email: string; updated_at: string }>
+  >(`${BASE_URL}/activity-groups`, { email: EMAIL, title: 'New Activity' });
+
+  if (response.status !== 201) {
+    throw new Error(
+      `Failed creating new activity groups: ${response.statusText}`
+    );
   }
 
   return response.data.data;
